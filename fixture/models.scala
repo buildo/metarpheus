51da9e5ced5d8dc703092084470c7a74d8c0c31c
Andrea Lattuada <utaaal@gmail.com>
2015-10-17 19:16:18 +0200

    Make fixtures into plain scala files so they can be used by integration tests

---
+++ fixture/models.scala
@@ -0,0 +1,12 @@
+package io.buildo.baseexample
+
+package models
+
+/**
+ * Represents a camping site
+ * @param name camping name
+ * @param size number of tents
+ */
+case class Camping(name: String, size: Int)
+""".stripMargin
+
