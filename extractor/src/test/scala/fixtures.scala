f7f4ca9fa9b21dd9f05310e8760382988dfb967e
Andrea Lattuada <utaaal@gmail.com>
2016-03-23 11:29:03 +0100

    Move fixtures to subdirectory

--- extractor/src/test/scala/fixtures.scala
+++ extractor/src/test/scala/fixtures.scala
@@ -1,6 +1,6 @@
 package morpheus
 
 object Fixtures {
-  val models = morpheus.util.slurp("fixture/models.scala")
-  val routes = morpheus.util.slurp("fixture/routes.scala")
+  val models = morpheus.util.slurp("fixture/sources/models.scala")
+  val routes = morpheus.util.slurp("fixture/sources/routes.scala")
 }
