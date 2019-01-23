fb397c1817a1c9acf32e24f8cbc66f3e072d34f4
Eric Camellini <eric.camellini@buildo.io>
2019-01-23 12:39:58 +0100

    Fixing formatting

--- core/shared/src/test/scala/io.buildo.metarpheus/core/extractors/ModelSuite.scala
+++ core/shared/src/test/scala/io.buildo.metarpheus/core/extractors/ModelSuite.scala
@@ -21,123 +21,123 @@ class ModelSuite extends FunSuite {
     import intermediate._
 
     val expected =
-        List(
-          CaseClass(
-            name = "CampingName",
-            members = List(
-              CaseClass.Member(name = "s", tpe = Type.Name("String"), desc = None)
-            ),
-            desc = None,
-            isValueClass = true,
+      List(
+        CaseClass(
+          name = "CampingName",
+          members = List(
+            CaseClass.Member(name = "s", tpe = Type.Name("String"), desc = None)
           ),
-          CaseClass(
-            name = "Camping",
-            members = List(
-              CaseClass.Member(name = "id", tpe = Type.Name("UUID"), desc = None),
-              CaseClass.Member(name = "name", tpe = Type.Name("CampingName"), desc = None),
-              CaseClass
-                .Member(name = "size", tpe = Type.Name("Int"), desc = Some("number of tents")),
-              CaseClass.Member(
-                name = "location",
-                tpe = Type.Name("CampingLocation"),
-                desc = Some("camping location")
-              ),
-              CaseClass.Member(
-                name = "rating",
-                tpe = Type.Name("CampingRating"),
-                desc = Some("camping rating")
-              ),
-              CaseClass.Member(
-                name = "a",
-                tpe = Type.Name("A"),
-                desc = None
-              )
+          desc = None,
+          isValueClass = true,
+        ),
+        CaseClass(
+          name = "Camping",
+          members = List(
+            CaseClass.Member(name = "id", tpe = Type.Name("UUID"), desc = None),
+            CaseClass.Member(name = "name", tpe = Type.Name("CampingName"), desc = None),
+            CaseClass
+              .Member(name = "size", tpe = Type.Name("Int"), desc = Some("number of tents")),
+            CaseClass.Member(
+              name = "location",
+              tpe = Type.Name("CampingLocation"),
+              desc = Some("camping location")
             ),
-            desc = Some("Represents a camping site"),
-            typeParams = List(
-              Type.Name("A")
+            CaseClass.Member(
+              name = "rating",
+              tpe = Type.Name("CampingRating"),
+              desc = Some("camping rating")
+            ),
+            CaseClass.Member(
+              name = "a",
+              tpe = Type.Name("A"),
+              desc = None
             )
           ),
-          CaseClass(
-            name = "Swan",
-            members = List(
-              CaseClass.Member(
-                name = "color",
-                tpe = Type.Name("String"),
-                desc = Some("color of the swan")
-              )
-            ),
-            desc = Some("Represents a swan")
-          ),
-          CaseEnum(
-            name = "CampingLocation",
-            values = List(
-              CaseEnum.Member(
-                name = "Seaside",
-                desc = Some("Near the sea")
-              ),
-              CaseEnum.Member(
-                name = "Mountains",
-                desc = Some("High up")
-              )
-            ),
-            desc = Some("Location of the camping site")
-          ),
-          CaseEnum(
-            name = "CampingRating",
-            values = List(
-              CaseEnum.Member(
-                name = "High",
-                desc = Some("High")
-              ),
-              CaseEnum.Member(
-                name = "Medium",
-                desc = Some("Medium")
-              ),
-              CaseEnum.Member(
-                name = "Low",
-                desc = Some("Low")
-              )
-            ),
-            desc = Some("Rating of the camping site")
-          ),
-          CaseEnum(
-            name = "Planet",
-            values = List(
-              CaseEnum.Member(
-                name = "Earth",
-                desc = Some("Earth is a blue planet")
-              ),
-              CaseEnum.Member(
-                name = "Another",
-                desc = Some("Not sure campings exist")
-              )
-            ),
-            desc = Some("Planet of the camping site")
-          ),
-          CaseEnum(
-            name = "Surface",
-            values = List(
-              CaseEnum.Member(
-                name = "Sand",
-                desc = Some("Sandy")
-              ),
-              CaseEnum.Member(
-                name = "Earth",
-                desc = Some("Dirt")
-              )
-            ),
-            desc = Some("Surface of the camping site")
-          ),
-          CaseClass(
-            name = "IgnoreMe",
-            members = List(
-              CaseClass.Member(name = "ignore", tpe = Type.Name("String"), desc = None)
-            ),
-            desc = None,
-            isValueClass = false,
+          desc = Some("Represents a camping site"),
+          typeParams = List(
+            Type.Name("A")
           )
+        ),
+        CaseClass(
+          name = "Swan",
+          members = List(
+            CaseClass.Member(
+              name = "color",
+              tpe = Type.Name("String"),
+              desc = Some("color of the swan")
+            )
+          ),
+          desc = Some("Represents a swan")
+        ),
+        CaseEnum(
+          name = "CampingLocation",
+          values = List(
+            CaseEnum.Member(
+              name = "Seaside",
+              desc = Some("Near the sea")
+            ),
+            CaseEnum.Member(
+              name = "Mountains",
+              desc = Some("High up")
+            )
+          ),
+          desc = Some("Location of the camping site")
+        ),
+        CaseEnum(
+          name = "CampingRating",
+          values = List(
+            CaseEnum.Member(
+              name = "High",
+              desc = Some("High")
+            ),
+            CaseEnum.Member(
+              name = "Medium",
+              desc = Some("Medium")
+            ),
+            CaseEnum.Member(
+              name = "Low",
+              desc = Some("Low")
+            )
+          ),
+          desc = Some("Rating of the camping site")
+        ),
+        CaseEnum(
+          name = "Planet",
+          values = List(
+            CaseEnum.Member(
+              name = "Earth",
+              desc = Some("Earth is a blue planet")
+            ),
+            CaseEnum.Member(
+              name = "Another",
+              desc = Some("Not sure campings exist")
+            )
+          ),
+          desc = Some("Planet of the camping site")
+        ),
+        CaseEnum(
+          name = "Surface",
+          values = List(
+            CaseEnum.Member(
+              name = "Sand",
+              desc = Some("Sandy")
+            ),
+            CaseEnum.Member(
+              name = "Earth",
+              desc = Some("Dirt")
+            )
+          ),
+          desc = Some("Surface of the camping site")
+        ),
+        CaseClass(
+          name = "IgnoreMe",
+          members = List(
+            CaseClass.Member(name = "ignore", tpe = Type.Name("String"), desc = None)
+          ),
+          desc = None,
+          isValueClass = false,
         )
+      )
     val comparison = DiffShow.diff[List[Model]](expected, result)
     assert(comparison.isIdentical, comparison.string)
   }
